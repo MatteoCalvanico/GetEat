8d130b175bd84d241f6593625ea14d60604cbc6f
Matteo <matteo.calvanico@studio.unibo.it>
2023-12-13 12:53:47 +0100

    Minor fix

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,9 @@
 <template>
     <main>
-        <section class="vh-100">
+        <section class="vh-100 d-flex align-items-center justify-content-center">
             <div class="container py-5 h-100">
                 <div class="row d-flex align-items-center justify-content-center h-100">
-                    <div class="col-md-6 col-lg-6 col-xl-6 offset-xl-1">
+                    <div class="col-md-6 col-lg-6 col-xl-6 offset-xl-1 text-center">
                         <div class="form-outline mb-4">
                             <img src="./assets/logo.png"
                             class="img-fluid" alt="GetEat">
