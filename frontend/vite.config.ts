9a2f5dfa44612ae5e43e5b53a4e68eb1e63b659c
Matteo <matteo.calvanico@studio.unibo.it>
2023-12-18 15:18:26 +0100

    minor fix for vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      "/api": "http://localhost:3000",
+      "/img": "http://localhost:3000"
+    }
+  }
 })
