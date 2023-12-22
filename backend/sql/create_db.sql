8b5e8a2d42baa9aed29ed3d418a5b40d4c5d43ab
Matteo <matteo.calvanico@studio.unibo.it>
2023-12-22 18:48:14 +0100

    Add ultimate SQL for create and insert

---
+++ backend/sql/create_db.sql
@@ -0,0 +1,62 @@
+DROP DATABASE IF EXISTS DBGetEat;
+CREATE DATABASE DBGetEat;
+USE DBGetEat;
+
+CREATE TABLE Categoria (
+  IDcat varchar(50) NOT NULL,
+  Nome varchar(50) NOT NULL,
+  Info varchar(100) NOT NULL,
+  Img varchar(100) NOT NULL,
+  PRIMARY KEY (IDcat)
+)
+ENGINE = InnoDB;
+
+CREATE TABLE Prodotto (
+  IDprod varchar(50) NOT NULL,
+  Nome varchar(50) NOT NULL,
+  Prezzo DECIMAL(10,2) NOT NULL,
+  Sconto DECIMAL(10,2) DEFAULT 0.00,
+  Img varchar(100) NOT NULL,
+  Kcal INTEGER NOT NULL,
+  Categoria varchar(50) NOT NULL,
+  PRIMARY KEY (IDprod),
+  UNIQUE INDEX ID_Prodotto_IND (IDprod ASC),
+  INDEX FKInventario_IND (Categoria ASC),
+  CONSTRAINT FKInventario_IND1
+    FOREIGN KEY (Categoria) REFERENCES Categoria(IDcat) ON DELETE NO ACTION ON UPDATE NO ACTION
+)
+ENGINE = InnoDB;
+
+CREATE TABLE Utente (
+  ID VARCHAR(50) NOT NULL,
+  Permesso VARCHAR(50) NOT NULL,
+  Username VARCHAR(50) NOT NULL,
+  Psw VARCHAR(50) NOT NULL,
+  PRIMARY KEY (ID),
+  UNIQUE INDEX ID_Utente_IND (ID)
+)
+ENGINE = InnoDB;
+
+CREATE TABLE Ordine (
+  IDordine varchar(50) NOT NULL,
+  Ordinatario varchar(50) NOT NULL,
+  PRIMARY KEY (IDordine),
+  UNIQUE INDEX ID_Ordine_IND (IDordine ASC),
+  INDEX FKAcquisto_IND (Ordinatario ASC),
+  CONSTRAINT FKAcquisto_IND1
+    FOREIGN KEY (Ordinatario) REFERENCES Utente(ID) ON DELETE NO ACTION ON UPDATE NO ACTION
+)
+ENGINE = InnoDB;
+
+CREATE TABLE Menu (
+  NumOrdine varchar(50) NOT NULL,
+  Prodotto varchar(50) NOT NULL,
+  PRIMARY KEY (Prodotto, NumOrdine),
+  UNIQUE INDEX ID_Menu_IND (Prodotto, NumOrdine ASC),
+  INDEX FKMen_Ord_IND (NumOrdine ASC),
+  CONSTRAINT FK_numordine
+    FOREIGN KEY (NumOrdine) REFERENCES Ordine(IDordine) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT FK_prodotto
+    FOREIGN KEY (Prodotto) REFERENCES Prodotto(IDprod) ON DELETE NO ACTION ON UPDATE NO ACTION
+)
+ENGINE = InnoDB;
---
+++ backend/sql/insert_data.sql
@@ -0,0 +1,35 @@
+
+-- Categoria
+INSERT INTO Categoria (IDcat, Nome, Info, Img) VALUES
+('cat1', 'Panini', 'Scegli il panino che più ti piace', 'catPanini.jpg'),
+('cat2', 'Dolci', 'Scegli il dolce che più ti piace', 'catDolci.jpg'),
+('cat3', 'Bevande', 'Scegli la bevanda che più ti piace', 'catBevande.jpg');
+
+-- Utente
+INSERT INTO Utente (ID, Permesso, Username, Psw) VALUES
+('u1', 'admin', 'admin', 'password'),
+('u2', 'user', 'utente1', 'password'),
+('u3', 'user', 'utente2', 'password');
+
+-- Prodotto
+INSERT INTO Prodotto (IDprod, Nome, Prezzo, Sconto, Img, Kcal, Categoria) VALUES
+('prod1', 'Panino al formaggio', 10.00, 0.00, 'prodPanino1.jpg', 1125, 'cat1'),
+('prod2', 'Panino alla porchetta', 15.00, 5.00, 'prodPanino2.jpg', 1500, 'cat1'),
+('prod3', 'Panino alla mortadella', 9.50, 2.00, 'prodPanino3.jpg', 1300, 'cat1'),
+('prod4', 'Torta al cioccolato [Fetta]', 2.45, 0.00, 'prodDolce1.jpg', 550, 'cat2'),
+('prod5', 'Tiramisù', 5.99, 0.00, 'prodDolce2.jpg', 250, 'cat2'),
+('prod6', 'Coca-Cola', 1.50, 0.00, 'prodBibita1.jpg', 140, 'cat3'),
+('prod7', 'Sprite', 1.50, 0.00, 'prodBibita2.jpg', 30, 'cat3');
+
+-- Ordine
+INSERT INTO Ordine (IDordine, Ordinatario) VALUES
+('1', 'u1'),
+('2', 'u1'),
+('3', 'u2');
+
+-- Menu
+INSERT INTO Menu (NumOrdine, Prodotto) VALUES
+('1', 'prod1'),
+('1', 'prod2'),
+('2', 'prod3'),
+('3', 'prod4');
