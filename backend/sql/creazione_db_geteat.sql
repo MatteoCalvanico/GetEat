c5044c72246d8f2c9b5e698289002105c9ac6f5c
Matteo <matteo.calvanico@studio.unibo.it>
2023-12-22 17:17:12 +0100

    Modify sql for dump data

--- backend/sql/creazione_db_geteat.sql
+++ backend/sql/creazione_db_geteat.sql
@@ -20,9 +20,9 @@ create table Categoria (
 -- Dumping data for table Categoria
 --
 
-INSERT INTO 'dbgeteat', 'categoria' ('IDcat', 'Nome', 'Info', 'Img') VALUES ('0001', 'Panini', 'Scegli il panino che più ti piace', 'catPanini.jpg');
-INSERT INTO 'dbgeteat', 'categoria' ('IDcat', 'Nome', 'Info', 'Img') VALUES ('0002', 'Dolci', 'Scegli il dolce che più ti piace', 'catDolci.jpg');
-INSERT INTO 'dbgeteat', 'categoria' ('IDcat', 'Nome', 'Info', 'Img') VALUES ('0003', 'Bevande', 'Scegli la bevanda che più ti piace', 'catBevande.jpg');
+INSERT INTO 'dbgeteat', 'categoria' ('IDcat', 'Nome', 'Info', 'Img') VALUES ('1', 'Panini', 'Scegli il panino che più ti piace', 'catPanini.jpg');
+INSERT INTO 'dbgeteat', 'categoria' ('IDcat', 'Nome', 'Info', 'Img') VALUES ('2', 'Dolci', 'Scegli il dolce che più ti piace', 'catDolci.jpg');
+INSERT INTO 'dbgeteat', 'categoria' ('IDcat', 'Nome', 'Info', 'Img') VALUES ('3', 'Bevande', 'Scegli la bevanda che più ti piace', 'catBevande.jpg');
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -37,10 +37,16 @@ create table Prodotto (
      constraint ID_Prodotto_ID primary key (IDprod));
 
 --
--- Dumping data for table Categoria
+-- Dumping data for table Prodotto
 --
 
-INSERT INTO 'dbgeteat', 'prodotto' ('IDprod', 'Nome', 'Prezzo', 'Sconto', 'Img', 'Kcal', 'Categoria') VALUES ();
+INSERT INTO 'dbgeteat', 'prodotto' ('IDprod', 'Nome', 'Prezzo', 'Sconto', 'Img', 'Kcal', 'Categoria') VALUES ('prod1', 'Panino al formaggio', '10.00', '0.00', 'prodPanino1.jpg', '1125', '1');
+INSERT INTO 'dbgeteat', 'prodotto' ('IDprod', 'Nome', 'Prezzo', 'Sconto', 'Img', 'Kcal', 'Categoria') VALUES ('prod2', 'Panino alla porchetta', '15.00', '5.00', 'prodPanino2.jpg', '1500'. '1');
+INSERT INTO 'dbgeteat', 'prodotto' ('IDprod', 'Nome', 'Prezzo', 'Sconto', 'Img', 'Kcal', 'Categoria') VALUES ('prod3', 'Panino alla mortadella', '9.50', '2.00', 'prodPanino3.jpg', '1300'. '1');
+INSERT INTO 'dbgeteat', 'prodotto' ('IDprod', 'Nome', 'Prezzo', 'Sconto', 'Img', 'Kcal', 'Categoria') VALUES ('prod4', 'Torta al cioccolato [Fetta]', '2.45', '0.00', 'prodDolce1.jpg', '550', '2');
+INSERT INTO 'dbgeteat', 'prodotto' ('IDprod', 'Nome', 'Prezzo', 'Sconto', 'Img', 'Kcal', 'Categoria') VALUES ('prod5', 'Tiramisù', '5.99', '0.00', 'prodDolce2.jpg', '', '2');
+INSERT INTO 'dbgeteat', 'prodotto' ('IDprod', 'Nome', 'Prezzo', 'Sconto', 'Img', 'Kcal', 'Categoria') VALUES ('prod6', 'Coca-Cola', '1.50', '0.00', 'prodBibita1.jpg', '140', '3');
+INSERT INTO 'dbgeteat', 'prodotto' ('IDprod', 'Nome', 'Prezzo', 'Sconto', 'Img', 'Kcal', 'Categoria') VALUES ('prod7', 'Sprite', '1.50', '0.00', 'prodBibita2.jpg', '30', '3');
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -50,7 +56,7 @@ create table Ordine (
      constraint ID_Ordine_ID primary key (IDordine));
 
 --
--- Dumping data for table Categoria
+-- Dumping data for table Ordine
 --
 
 INSERT INTO 'dbgeteat', 'Ordine' ('IDordine', 'Ordinatario') VALUES ();
@@ -63,7 +69,7 @@ create table Menu (
      constraint ID_Menu_ID primary key (Prodotto, NumOrdine));
 
 --
--- Dumping data for table Categoria
+-- Dumping data for table Menu
 --
 
 INSERT INTO 'dbgeteat', 'Menu' ('NumOrdine', 'Prodotto') VALUES ();
@@ -78,10 +84,12 @@ create table Utente (
      constraint ID_Utente_ID primary key (ID));
 
 --
--- Dumping data for table Categoria
+-- Dumping data for table Utente
 --
 
-INSERT INTO 'dbgeteat', 'Utente' ('ID', 'Permesso', 'Username', 'Psw') VALUES ();
+INSERT INTO 'dbgeteat', 'Utente' ('ID', 'Permesso', 'Username', 'Psw') VALUES ('1', 'admin', 'admin', 'password');
+INSERT INTO 'dbgeteat', 'Utente' ('ID', 'Permesso', 'Username', 'Psw') VALUES ('2', 'user', 'utente1', 'password');
+INSERT INTO 'dbgeteat', 'Utente' ('ID', 'Permesso', 'Username', 'Psw') VALUES ('3', 'user', 'utente2', 'password');
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
