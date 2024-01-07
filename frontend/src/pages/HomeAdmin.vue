93769e51d7886067f99e8e7cbd79ceef499d82c7
Matteo <matteo.calvanico@studio.unibo.it>
2024-01-07 16:02:29 +0100

    Now all the orders are group by ID

--- frontend/src/pages/HomeAdmin.vue
+++ frontend/src/pages/HomeAdmin.vue
@@ -1,38 +1,56 @@
 <script lang="ts">
-    import { defineComponent } from 'vue'
-    import axios from "axios"
-    import { Ordine } from '../types'
+import { defineComponent } from 'vue';
+import axios from 'axios';
+import { Ordine } from '../types';
 
-    export default defineComponent({
-        data() {
-            return {
-                datiOrdini: [] as Ordine []
-            }
-        },
-        methods: {
-            getOrdini(){
-                axios.get("/api/ordini").then(response => this.datiOrdini = response.data)
-            }
-        },
-        mounted(){
-            this.getOrdini()
+export default defineComponent({
+  data() {
+    return {
+      datiOrdini: [] as Ordine[][],
+    };
+  },
+  methods: {
+    getOrdini() {
+      axios.get('/api/ordini').then((response) => {
+        this.datiOrdini = this.groupByOrderID(response.data);
+      });
+    },
+    groupByOrderID(ordini: Ordine[]){
+      const OrdGroupBy: Record<number, Ordine[]> = {}; //Utilizzando un Record, funziona come una 'map' ma meno complicata da usare, che ci permette di organizzare per bene tutti gli ordini con la stessa chiave
+      ordini.forEach((ordine) => {
+        const key = ordine.IDordine;
+        if (!OrdGroupBy[key]) { //Se la chiave non è ancora presente nella lista la aggiungiamo
+          OrdGroupBy[key] = [];
         }
-    })
+        OrdGroupBy[key].push(ordine); //Assocciamo l'ordine attuale alla chiave giusta
+      });
+      return Object.values(OrdGroupBy); //Ritorniamo un array di ordini (v-for non funziona altrimento)
+    },
+  },
+  mounted() {
+    this.getOrdini();
+  },
+});
 </script>
 
 <template>
-    <div class="container">
-        <h1>Pagina Admin</h1>
-        <div class="row">
-            <div class="text-center">
-                <ul>
-                    <li v-for="ordine in datiOrdini">
-                        ID: {{ ordine.IDordine }} - Ordinatario: {{ ordine.NomeOrdinatario }} - Prodotto: {{ ordine.NomeProdotto }}
-                    </li>
-                </ul>
-            </div>
-        </div>
+  <div class="container">
+    <h1>Pagina Admin</h1>
+    <div class="row">
+      <div class="text-center">
+        <ul>
+          <li v-for="ordGroup in datiOrdini" :key="ordGroup[0].IDordine">
+            ID: {{ ordGroup[0].IDordine }}
+            <ul>
+              <li v-for="ordine in ordGroup" :key="ordine.IDordine">
+                - Prodotto: {{ ordine.NomeProdotto }}
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
+  </div>
 </template>
 
 <style lang="scss">
