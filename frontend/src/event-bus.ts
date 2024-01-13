61e992ddd930a20448f08d9db797efff28841e2c
Matteo <matteo.calvanico@studio.unibo.it>
2024-01-13 09:21:06 +0100

    Modify README

--- frontend/src/event-bus.ts
+++ frontend/src/event-bus.ts
@@ -1,4 +1,4 @@
-//Uso un bus degli eventi di Vue per condividere il carrello fra più componenti
+//Uso un bus degli eventi per condividere il carrello fra più componenti
 
 import { ref } from 'vue'; //utile per ottenere un riferimento diretto a uno specifico elemento
 export const eventBus = ref(new EventTarget());
