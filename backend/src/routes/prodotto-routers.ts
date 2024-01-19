65a0e77df1bc493ace10528d0d0deba1e3f9c60e
Matteo <matteo.calvanico@studio.unibo.it>
2024-01-19 17:23:00 +0100

    Some change in the multer part

--- backend/src/routes/prodotto-routers.ts
+++ backend/src/routes/prodotto-routers.ts
@@ -3,7 +3,7 @@ import multer from "multer";  //Usiamo 'multer' per gestire le immagini
 import * as path from "path"
 import * as prodController from "../controllers/prodotto-controller";
 
-//Multer part
+//Multer part START
 const imgLocation = path.join(__dirname, '../../public/img');
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -14,7 +14,7 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ storage: storage });
-
+//Multer part END
 
 const router: Router = Router();
 
